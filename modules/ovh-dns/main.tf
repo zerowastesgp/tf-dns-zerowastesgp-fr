24b7c10a926b1d3aecd74236711fca396478558d
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-08-22 01:05:40 +0100

    Upgrade to Terraform 0.13

--- modules/ovh-dns/main.tf
+++ modules/ovh-dns/main.tf
@@ -1,9 +1,12 @@
 # https://www.terraform.io/docs/configuration/terraform.html#terraform-block-syntax.
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13.0"
 
   required_providers {
     # https://www.terraform.io/docs/providers/ovh/.
-    ovh = "~> 0.6"
+    ovh = {
+      source  = "terraform-providers/ovh"
+      version = "~> 0.6"
+    }
   }
 }
